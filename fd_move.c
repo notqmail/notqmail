cb274e19e432f6e622e22f5c8d2a3da761336d79
Alan Post <adp@prgmr.com>
2019-07-22 08:20:54 -0600

    fd_copy.c, fd_move,c, fd.h function declarations are now C89.

--- fd_move.c
+++ fd_move.c
@@ -1,8 +1,7 @@
+#include <unistd.h>
 #include "fd.h"
 
-int fd_move(to,from)
-int to;
-int from;
+int fd_move(int to,int from)
 {
   if (to == from) return 0;
   if (fd_copy(to,from) == -1) return -1;

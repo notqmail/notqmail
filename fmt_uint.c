72c58ae8b2b8d5185449c120fd0844be1933a899
Leah Neukirchen <leah@vuxu.org>
2020-04-21 20:54:17 +0200

    fmt_*.c: convert to prototypes

--- fmt_uint.c
+++ fmt_uint.c
@@ -1,6 +1,6 @@
 #include "fmt.h"
 
-unsigned int fmt_uint(s,u) register char *s; register unsigned int u;
+unsigned int fmt_uint(char *s, unsigned int u)
 {
   register unsigned long l; l = u; return fmt_ulong(s,l);
 }
--- fmt_uint0.c
+++ fmt_uint0.c
@@ -1,6 +1,6 @@
 #include "fmt.h"
 
-unsigned int fmt_uint0(s,u,n) char *s; unsigned int u; unsigned int n;
+unsigned int fmt_uint0(char *s, unsigned int u, unsigned int n)
 {
   unsigned int len;
   len = fmt_uint(FMT_LEN,u);

aac1dcd5c221bb7673d5c61346bff8d87c96d300
Rolf Eike Beer <eike@sf-mail.de>
2020-05-24 18:15:00 +0200

    add parameters to case.h declarations
    While at it change the function API to C89 type and remove register
    specifications.

--- case.h
+++ case.h
@@ -4,11 +4,11 @@
 #include <string.h>
 #include <strings.h>
 
-extern void case_lowers();
-extern void case_lowerb();
+extern void case_lowers(char *s);
+extern void case_lowerb(char *s, unsigned int len);
 #define case_diffs(s,t) (strcasecmp((s),(t)) != 0)
-extern int case_diffb();
-extern int case_starts();
+extern int case_diffb(char *s, unsigned int len, char *t);
+extern int case_starts(char *s, char *t);
 extern int case_startb();
 
 #define case_equals(s,t) (!case_diffs((s),(t)))

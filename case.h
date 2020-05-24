2fb582a56efccd8172c44bb383035dff4b2b6147
Rolf Eike Beer <eike@sf-mail.de>
2020-05-24 18:18:12 +0200

    remove stale declaration case_startb() from case.h and case.3
    No implementation has ever existed in the code.

--- case.h
+++ case.h
@@ -9,7 +9,6 @@ extern void case_lowerb(char *s, unsigned int len);
 #define case_diffs(s,t) (strcasecmp((s),(t)) != 0)
 extern int case_diffb(char *s, unsigned int len, char *t);
 extern int case_starts(char *s, char *t);
-extern int case_startb();
 
 #define case_equals(s,t) (!case_diffs((s),(t)))
 

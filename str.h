10f93f9bece921983a0a63952e3830a16ec59ca6
Rolf Eike Beer <eike@sf-mail.de>
2020-05-24 15:24:22 +0200

    replace str_rchr() with strrchr()

--- str.h
+++ str.h
@@ -8,7 +8,15 @@
 #define str_diffn(s,t,len) strncmp((s),(t),(len))
 #define str_len(s) strlen((s))
 extern unsigned int str_chr();
-extern unsigned int str_rchr();
+
+static inline unsigned int str_rchr(const char *s, int c)
+{
+  const char *t = strrchr(s, c);
+  if (!t)
+    return strlen(s);
+  return t - s;
+}
+
 static inline int str_start(const char *s, const char *t)
 {
   return strncmp(s, t, strlen(t)) == 0;

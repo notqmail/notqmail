f46913371291354772f53cd6db507509a356d1ec
Rolf Eike Beer <eike@sf-mail.de>
2020-06-15 21:14:36 +0200

    allow additional groups for qmail-remote
    Common patches add e.g. DKIM signing or TLS encryption also for remote
    processes. Until now you could only have these files belong to group qmail,
    which collides with the claim "these programs don't even trust each other" from
    SECURITY.md, or make them world readable, which is even worse.
    
    Honor all groups the qmailr user is part of when spawning qmail-rspawn. This
    allows constructs like:
    
    -primary group qmail: the queue can be read
    -secondary group qmailcert: the TLS certificates can be read, and qmail-smtpd
     can share that group membership
    -third group qmailr: the DKIM keys

--- prot.c
+++ prot.c
@@ -1,11 +1,30 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <grp.h>
 #include "prot.h"
+#include <sys/types.h>
+#include <grp.h>
+#include <unistd.h>
 
-int prot_gid(gid_t gid)
+#ifndef GROUP_COUNT
+/* use a sane default, but one can override it from outside if there are
+ * higher requirements. */
+#define GROUP_COUNT 8
+#endif
+int prot_gids(const char *user, gid_t gid)
 {
-  if (setgroups(1,&gid) == -1) return -1;
+  gid_t gids[GROUP_COUNT];
+  int gcount = GROUP_COUNT;
+
+  if (user) {
+    int r = getgrouplist(user, gid, gids, &gcount);
+    /* member of too many groups */
+    if (r < 0) {
+      return r;
+    }
+  } else {
+    gcount = 1;
+    gids[0] = gid;
+  }
+
+  if (setgroups(gcount,gids) == -1) return -1;
   return setgid(gid); /* _should_ be redundant, but on some systems it isn't */
 }
 
--- prot.h
+++ prot.h
@@ -1,7 +1,10 @@
 #ifndef PROT_H
 #define PROT_H
 
-extern int prot_gid();
-extern int prot_uid();
+#include <sys/types.h>
+
+#define prot_gid(gid) prot_gids(NULL,(gid))
+extern int prot_gids(const char *user, gid_t gid);
+extern int prot_uid(uid_t uid);
 
 #endif

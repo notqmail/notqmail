1fd3f91cf93e96be9bc9b8c6e7babc769fda359d
Rolf Eike Beer <eike@sf-mail.de>
2020-06-17 19:37:59 +0200

    reduce prot_uid() to a define

--- prot.h
+++ prot.h
@@ -2,9 +2,10 @@
 #define PROT_H
 
 #include <sys/types.h>
+#include <unistd.h>
 
 extern int prot_gid(gid_t gid);
 extern int prot_gids(const char *user, gid_t gid);
-extern int prot_uid(uid_t uid);
+#define prot_uid(uid) setuid(uid)
 
 #endif

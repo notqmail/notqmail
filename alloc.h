b52f6d20817fd850144b087bcc668407947dfb1a
Rolf Eike Beer <eike@sf-mail.de>
2020-06-20 09:45:03 +0200

    reintroduce alloc_re()
    This is used by the large-dns patch, and may be used by others. Add it back in
    a way that it will not be compiled in our normal build, but a simple compiler
    define allows external patches to find the needed environment.

--- alloc.h
+++ alloc.h
@@ -6,4 +6,27 @@
 #define alloc(x) malloc(x)
 #define alloc_free(x) free(x)
 
+/*
+ * In-tree code must not call this deprecated function. It's here because
+ * some external patches still do. We intend to remove it as soon as is
+ * practical.
+ */
+#ifndef DONT_PROVIDE_OBSOLETE
+
+#if defined(__clang__) || defined(__GNUC__)
+#define _deprecated_ __attribute__((deprecated))
+#else
+#define _deprecated_
+#endif
+
+static inline int _deprecated_ alloc_re(void **x, unsigned int m, unsigned int n)
+{
+  void *y = realloc(*x, n);
+  (void)m;
+  if (y != NULL)
+    *x = y;
+  return !!y;
+}
+#endif
+
 #endif

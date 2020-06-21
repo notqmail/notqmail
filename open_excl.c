60330c8f658674c6b61e55c7c4b1f635bab59bdc
Rolf Eike Beer <eike@sf-mail.de>
2020-06-21 13:32:11 +0200

    constify filename arguments passed to open_*()

--- open_excl.c
+++ open_excl.c
@@ -2,5 +2,5 @@
 #include <fcntl.h>
 #include "open.h"
 
-int open_excl(fn) char *fn;
+int open_excl(const char *fn)
 { return open(fn,O_WRONLY | O_EXCL | O_CREAT,0644); }
--- open_trunc.c
+++ open_trunc.c
@@ -2,5 +2,5 @@
 #include <fcntl.h>
 #include "open.h"
 
-int open_trunc(fn) char *fn;
+int open_trunc(const char *fn)
 { return open(fn,O_WRONLY | O_NDELAY | O_TRUNC | O_CREAT,0644); }

4ed67e08960faeb877519f75abcc6efeab38c68d
Rolf Eike Beer <eike@sf-mail.de>
2020-06-21 13:32:11 +0200

    constify filename arguments passed to open_*()

--- open.h
+++ open.h
@@ -1,10 +1,10 @@
 #ifndef OPEN_H
 #define OPEN_H
 
-extern int open_read();
-extern int open_excl();
-extern int open_append();
-extern int open_trunc();
-extern int open_write();
+extern int open_read(const char *fn);
+extern int open_excl(const char *fn);
+extern int open_append(const char *fn);
+extern int open_trunc(const char *fn);
+extern int open_write(const char *fn);
 
 #endif
--- open_append.c
+++ open_append.c
@@ -2,5 +2,5 @@
 #include <fcntl.h>
 #include "open.h"
 
-int open_append(fn) char *fn;
+int open_append(const char *fn)
 { return open(fn,O_WRONLY | O_NDELAY | O_APPEND | O_CREAT,0600); }

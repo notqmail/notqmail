c8c4b2b5f37b4f9a477d0a2ded1e4c2cfc5137d1
Rolf Eike Beer <eike@sf-mail.de>
2020-06-21 13:34:48 +0200

    constify filename arguments passed to control_*()
    While at it do the same for the default value passed to control_rldef().

--- control.h
+++ control.h
@@ -1,10 +1,12 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 
-extern int control_init();
-extern int control_readline();
-extern int control_rldef();
-extern int control_readint();
-extern int control_readfile();
+#include "stralloc.h"
+
+extern int control_init(void);
+extern int control_readline(stralloc *sa, const char *fn);
+extern int control_rldef(stralloc *sa, const char *fn, int flagme, const char *def);
+extern int control_readint(int *i, const char *fn);
+extern int control_readfile(stralloc *sa, const char *fn, int flagme);
 
 #endif

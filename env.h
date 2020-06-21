4eabbbba26ca332e1c4546b84accf588469d01b7
Rolf Eike Beer <eike@sf-mail.de>
2020-06-21 14:03:44 +0200

    constify arguments to env_*()

--- env.h
+++ env.h
@@ -4,13 +4,13 @@
 extern int env_isinit;
 
 extern int env_init();
-extern int env_put();
-extern int env_put2();
-extern int env_unset();
-extern /*@null@*/char *env_get();
+extern int env_put(const char *s);
+extern int env_put2(const char *s, const char *t);
+extern int env_unset(const char *s);
+extern /*@null@*/char *env_get(const char *s);
 extern char *env_pick();
 extern void env_clear();
-extern char *env_findeq();
+extern const char *env_findeq(const char *s);
 
 extern char **environ;
 

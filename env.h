7895a75e7b5669f02a76c91690b49b2fc201f168
Rolf Eike Beer <eike@sf-mail.de>
2020-06-21 14:05:56 +0200

    remove unused function env_pick()

--- env.h
+++ env.h
@@ -8,7 +8,6 @@ extern int env_put(const char *s);
 extern int env_put2(const char *s, const char *t);
 extern int env_unset(const char *s);
 extern /*@null@*/char *env_get(const char *s);
-extern char *env_pick();
 extern void env_clear();
 extern const char *env_findeq(const char *s);
 

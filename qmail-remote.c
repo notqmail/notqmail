3175f621c29d3647577d6d670074934a20860d66
Rolf Eike Beer <eike@sf-mail.de>
2020-11-21 17:22:18 +0100

    split smtpcode() out of qmail-remote.c

--- qmail-remote.c
+++ qmail-remote.c
@@ -24,11 +24,10 @@
 #include "constmap.h"
 #include "tcpto.h"
 #include "readwrite.h"
+#include "smtpcode.h"
 #include "timeoutconn.h"
 #include "timeoutread.h"
 #include "timeoutwrite.h"
-
-#define HUGESMTPTEXT 5000
 
 #define PORT_SMTP 25 /* silly rabbit, /etc/services is for users */
 unsigned long port = PORT_SMTP;
@@ -129,38 +128,6 @@ char smtpfrombuf[128];
 substdio smtpfrom = SUBSTDIO_FDBUF(saferead,-1,smtpfrombuf,sizeof smtpfrombuf);
 
 stralloc smtptext = {0};
-
-static void get(unsigned char *uc)
-{
-  char *ch = (char *)uc;
-  substdio_get(&smtpfrom,ch,1);
-  if (*ch != '\r')
-    if (smtptext.len < HUGESMTPTEXT)
-     if (!stralloc_append(&smtptext,ch)) temp_nomem();
-}
-
-unsigned long smtpcode()
-{
-  unsigned char ch;
-  unsigned long code;
-
-  if (!stralloc_copys(&smtptext,"")) temp_nomem();
-
-  get(&ch); code = ch - '0';
-  get(&ch); code = code * 10 + (ch - '0');
-  get(&ch); code = code * 10 + (ch - '0');
-  for (;;) {
-    get(&ch);
-    if (ch != '-') break;
-    while (ch != '\n') get(&ch);
-    get(&ch);
-    get(&ch);
-    get(&ch);
-  }
-  while (ch != '\n') get(&ch);
-
-  return code;
-}
 
 void outsmtptext()
 {
---
+++ smtpcode.c
@@ -0,0 +1,41 @@
+#include "smtpcode.h"
+
+#include "substdio.h"
+#include "stralloc.h"
+
+#define HUGESMTPTEXT 5000
+
+extern stralloc smtptext;
+extern substdio smtpfrom;
+
+static void get(unsigned char *uc)
+{
+  char *ch = (char *)uc;
+  substdio_get(&smtpfrom,ch,1);
+  if (*ch != '\r')
+    if (smtptext.len < HUGESMTPTEXT)
+     if (!stralloc_append(&smtptext,ch)) temp_nomem();
+}
+
+unsigned long smtpcode()
+{
+  unsigned char ch;
+  unsigned long code;
+
+  if (!stralloc_copys(&smtptext,"")) temp_nomem();
+
+  get(&ch); code = ch - '0';
+  get(&ch); code = code * 10 + (ch - '0');
+  get(&ch); code = code * 10 + (ch - '0');
+  for (;;) {
+    get(&ch);
+    if (ch != '-') break;
+    while (ch != '\n') get(&ch);
+    get(&ch);
+    get(&ch);
+    get(&ch);
+  }
+  while (ch != '\n') get(&ch);
+
+  return code;
+}

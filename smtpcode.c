d910d912a9b8d801865ab229875c543cefdfe8a5
Rolf Eike Beer <eike@sf-mail.de>
2020-11-21 19:03:26 +0100

    qmail-remote: verify the SMTP result code of remote servers much stricter
    The server reply must be of the form
    
      (\d\d\d-[^\r\n]*\r\n)*
      \d\d\d [^\r\n]*\r\n*
    
    Until now it was neither verified that the first 3 characters were digits, nor
    that the following character was really SPACE or MINUS. In case any of these
    restrictions violated return a high, invalid, SMTP code, that will cause all
    callers to return permanent error.
    
    While at it make sure noone can send an absurdly low number as status code,
    and return an error in that case, too.

--- smtpcode.c
+++ smtpcode.c
@@ -17,25 +17,50 @@ static void get(unsigned char *uc)
      if (!stralloc_append(&smtptext,ch)) temp_nomem();
 }
 
+static int get_digit(unsigned char *uc)
+{
+  get(uc);
+  if (*uc < '0' || *uc > '9')
+    return 1;
+  return 0;
+}
+
 unsigned long smtpcode()
 {
   unsigned char ch;
   unsigned long code;
+  int err = 0;
 
   if (!stralloc_copys(&smtptext,"")) temp_nomem();
 
-  get(&ch); code = ch - '0';
-  get(&ch); code = code * 10 + (ch - '0');
-  get(&ch); code = code * 10 + (ch - '0');
+  err += get_digit(&ch); code = ch - '0';
+  /* valid SMTP codes are >= 200 */
+  if (ch < '2')
+    err++;
+  err += get_digit(&ch); code = code * 10 + (ch - '0');
+  err += get_digit(&ch); code = code * 10 + (ch - '0');
+
   for (;;) {
     get(&ch);
+    if (ch != ' ' && ch != '-')
+      err++;
     if (ch != '-') break;
     while (ch != '\n') get(&ch);
-    get(&ch);
-    get(&ch);
-    get(&ch);
+    err += get_digit(&ch);
+    if (ch - '0' != code / 100)
+      err++;
+    err += get_digit(&ch);
+    if (ch - '0' != (code / 10) % 10)
+      err++;
+    err += get_digit(&ch);
+    if (ch - '0' != code % 10)
+      err++;
   }
   while (ch != '\n') get(&ch);
 
+  /* Make an invalid response code look like a permanent error (>500) to callers
+   * without letting it be mistaken for a real 3-digit response code. */
+  if (err)
+    return 9999;
   return code;
 }

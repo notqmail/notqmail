f6ddc55ba42a133d641705c88d94abe1cf0b2ff4
Rolf Eike Beer <eike@sf-mail.de>
2020-12-04 22:10:16 +0100

    constify stralloc_*() function arguments
    While at it reduce 2 of them to inline.

--- stralloc.h
+++ stralloc.h
@@ -2,6 +2,7 @@
 #define STRALLOC_H
 
 #include "gen_alloc.h"
+#include <string.h>
 
 GEN_ALLOC_typedef(stralloc,char,s,len,a)
 
@@ -9,12 +10,18 @@ extern int stralloc_ready();
 extern int stralloc_readyplus();
 extern int stralloc_copy();
 extern int stralloc_cat();
-extern int stralloc_copys();
-extern int stralloc_cats();
-extern int stralloc_copyb();
-extern int stralloc_catb();
+extern int stralloc_copyb(stralloc *sa, const char *s, unsigned int n);
+extern int stralloc_catb(stralloc *sa, const char *s, unsigned int n);
 extern int stralloc_append(); /* beware: this takes a pointer to 1 char */
-extern int stralloc_starts();
+extern int stralloc_starts(stralloc *sa, const char *s);
+static inline int stralloc_cats(stralloc *sa, const char *s)
+{
+  return stralloc_catb(sa,s,strlen(s));
+}
+static inline int stralloc_copys(stralloc *sa, const char *s)
+{
+  return stralloc_copyb(sa,s,strlen(s));
+}
 
 #define stralloc_0(sa) stralloc_append(sa,"")
 

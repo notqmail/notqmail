792a8597fcb25f4332be0d2058a0372b1a36dae4
Rolf Eike Beer <eike@sf-mail.de>
2020-12-04 21:49:09 +0100

    constify log function arguments in qsutil.h

--- qsutil.h
+++ qsutil.h
@@ -3,20 +3,20 @@
 
 #include "deprecated.h"
 
-extern void log1();
-extern void qslog2(char *, char *);
+extern void log1(const char *);
+extern void qslog2(const char *, const char *);
 
 #ifdef DEPRECATED_FUNCTIONS_AVAILABLE
-static inline void _deprecated_ log2(char *s1, char *s2)
+static inline void _deprecated_ log2(const char *s1, const char *s2)
 {
   qslog2(s1,s2);
 }
 #endif
 
-extern void log3();
+extern void log3(const char *s1, const char *s2, const char *s3);
 extern void logsa();
 extern void nomem();
-extern void pausedir();
-extern void logsafe();
+extern void pausedir(const char *dir);
+extern void logsafe(const char *s);
 
 #endif

notqmail/notqmail

commit 95f86dafbb94c00dbbecd01fadb7c0c659aafe51

Let NROFF be redefined in the environment.

Makefile
@@ -1,7 +1,7 @@
 # Don't edit Makefile! Use conf-* for configuration.
 
 SHELL=/bin/sh
-NROFF=nroff
+NROFF?=nroff
 
 default: it
 

.travis.yml
@@ -4,7 +4,7 @@ env:
   matrix:
     - BUILD_NAME="default"
     - BUILD_NAME="utmp" FORCE_UTMP="1" MAKEFLAGS="-o qtmp.h"
-    - BUILD_NAME="no_catman" MAKEFLAGS="NROFF=true"
+    - BUILD_NAME="no_catman" AVOID_NROFF="1"
 compiler:
  - gcc
  - clang
@@ -16,6 +16,7 @@ addons:
 script:
  - echo "${CC} -O2 -Wall -Wshadow" > conf-cc
  - if [ -n "${FORCE_UTMP}" ]; then cp qtmp.h1 qtmp.h; fi
+ - if [ -n "${AVOID_NROFF}" ]; then NROFF=true; export NROFF; fi
  - make ${MAKEFLAGS} it man
  - make test
  - env DESTDIR=/tmp/qmail make ${MAKEFLAGS} package

commit 6c12e8a4b378ead9c6450f7eaf966dc5c9774c8c

Always define NROFF to work around Solaris make(1).
On at least Solaris 11's make(1), "NROFF?=nroff" means the suffix rules
for generating preformatted manpages somehow get an empty value for
NROFF. Setting "NROFF=nroff" fixes this behavior.

This change means "NROFF=true make man" no longer overrides the value
of NROFF, but that's okay: "make NROFF=true man" still does. Our own
autobuilds already do the latter.

Fixes: 95f86dafbb94c00dbbecd01fadb7c0c659aafe51

Makefile
@@ -1,7 +1,7 @@
 # Don't edit Makefile! Use conf-* for configuration.
 
 SHELL=/bin/sh
-NROFF?=nroff
+NROFF=nroff
 
 default: it
 
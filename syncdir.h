15589560921f43f93cba8b145995b4390449d9a2
Amitai Schleier <schmonz-web-git@schmonz.com>
2022-02-07 21:03:46 +0100

    Comment.

--- syncdir.h
+++ syncdir.h
@@ -1,8 +1,10 @@
 #ifndef SYNCDIR_H
 #define SYNCDIR_H
 
+/* internal */
 int fdirsyncfn(const char *);
 
+/* call these wrappers instead of the syscalls directly */
 int syncdir_open(const char *, const int);
 int syncdir_link(const char *, const char *);
 int syncdir_unlink(const char *);

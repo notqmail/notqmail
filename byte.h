f301756e28474ec103ff9f932d7e566775320229
Rolf Eike Beer <eike@sf-mail.de>
2024-04-26 21:46:30 +0200

    make input argument of byte_copyr() const

--- byte.h
+++ byte.h
@@ -12,7 +12,7 @@ static inline unsigned int byte_chr(const void *s, unsigned int n, int c)
 }
 extern unsigned int byte_rchr(char *s, unsigned int n, int c);
 #define byte_copy(to,n,from) memcpy(to,from,n)
-extern void byte_copyr(char *to, unsigned int n, char *from);
+extern void byte_copyr(char *to, unsigned int n, const char *from);
 extern void byte_zero(void *m, unsigned int n);
 
 #define byte_equal(s,n,t) (memcmp((s),(t),(n)) == 0)
--- byte_cr.c
+++ byte_cr.c
@@ -1,6 +1,6 @@
 #include "byte.h"
 
-void byte_copyr(char *to, unsigned int n, char *from)
+void byte_copyr(char *to, unsigned int n, const char *from)
 {
   to += n;
   from += n;

2b7780f26c14a75dc5c7cbf54969be37827620ec
Rolf Eike Beer <eike@sf-mail.de>
2024-04-28 10:48:42 +0200

    byte_zero: take void* as pointer argument
    Any type of memory get's passed here, and newer compilers don't like the missing
    typecast.

--- byte.h
+++ byte.h
@@ -12,8 +12,8 @@ static inline unsigned int byte_chr(const void *s, unsigned int n, int c)
 }
 extern unsigned int byte_rchr();
 #define byte_copy(to,n,from) memcpy(to,from,n)
-extern void byte_copyr();
 extern void byte_zero();
+extern void byte_zero(void *m, unsigned int n);
 
 #define byte_equal(s,n,t) (memcmp((s),(t),(n)) == 0)
 
--- byte_zero.c
+++ byte_zero.c
@@ -1,7 +1,8 @@
 #include "byte.h"
 
-void byte_zero(char *s, unsigned int n)
+void byte_zero(void *m, unsigned int n)
 {
+  char *s;
   for (;;) {
     if (!n) break; *s++ = 0; --n;
     if (!n) break; *s++ = 0; --n;
